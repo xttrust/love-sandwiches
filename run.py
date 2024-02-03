8364ed8f0a2264411985bb350d0b2c070faf7e84
xttrust <xttrust@live.com>
2024-02-03 04:27:36 +0000

    Create get_sales_data function to get data string from user

--- run.py
+++ run.py
@@ -16,4 +16,15 @@ sales = SHEET.worksheet('sales')
 
 data = sales.get_all_values()
 
-print(data)
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be xis numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+get_sales_data()
